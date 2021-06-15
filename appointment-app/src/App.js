12ad687ffe59d5e7e8c8e292c74e77ef495bf1f8
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-06-16 01:55:19 +0500

    Added routes

--- appointment-app/src/App.js
+++ appointment-app/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom'
+import Dashboard from './components/Dashboard/Dashboard';
 import Login from './components/Login/Login'
+import Signup from './components/Signup/Signup';
 
 
 function App() {
   return (
     <Switch>
+        <Route exact path="/" component={Dashboard} />
         <Route path="/login" component={Login}/>
+        <Route path="/signup" component={Signup} />
     </Switch>
 
   );
