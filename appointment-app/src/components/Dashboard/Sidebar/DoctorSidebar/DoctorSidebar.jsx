6a7e754f76015faa5e2233ef4432e39e5ae2e324
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-06-18 22:51:32 +0500

    Added Doctor Sidebar

---
+++ appointment-app/src/components/Dashboard/Sidebar/DoctorSidebar/DoctorSidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Layout } from 'antd'
+import './styles.scss'
+
+const DoctorSideBar = () =>{
+    const {Sider} = Layout
+    return(
+        <Sider>
+            <div>
+                Sider check
+            </div>
+        </Sider>
+    )
+}
+
+export default DoctorSideBar
