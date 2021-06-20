bf0c854da051b7fc2c0a0346c198d66f71f1a2eb
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-06-20 19:06:32 +0500

    RenderDashboard

--- appointment-app/src/components/Dashboard/DoctorDashboard/DoctorDashboardContent/DoctorDashboardContent.jsx
+++ appointment-app/src/components/Dashboard/DoctorDashboard/DoctorDashboardContent/DoctorDashboardContent.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import { Layout } from 'antd'
+import DashboardHome from './DashboardHome/DashboardHome'
+import DashboardAppointment  from './DashboardAppointment/DashboardAppointment'
+import DashboardPatient from './DashboardPatient/DashboardPatient'
 
-const DoctorDashboardContent = () =>{
+const DoctorDashboardContent = ({current}) =>{
     const {Content} = Layout
+    console.log('current', current)
+    const renderDashboardComponenet = () =>{
+        switch(current) {
+            case 'dashboard' :
+                return <DashboardHome />
+            case 'patients' :
+                return <DashboardPatient />
+            case 'appointment' : 
+                return <DashboardAppointment /> 
+            default:
+                return <></>
+        }
+    }
     return(
        <Content>
-           <div>Doc content</div>
+           {renderDashboardComponenet()}
        </Content>
     )
 }
