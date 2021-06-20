0b2cc158eee706249c01a585e5b85a873dac2b9a
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-06-20 19:06:18 +0500

    Implemented DoctorSidebar

--- appointment-app/src/components/Dashboard/Sidebar/DoctorSidebar/DoctorSidebar.jsx
+++ appointment-app/src/components/Dashboard/Sidebar/DoctorSidebar/DoctorSidebar.jsx
@@ -1,13 +1,71 @@
-import React from 'react'
-import { Layout } from 'antd'
+import React, { useContext } from 'react'
+import { Layout, Avatar, Menu, Button } from 'antd'
+import {UserOutlined, MailOutlined, PhoneOutlined, HomeFilled, CalendarFilled} from '@ant-design/icons'
 import './styles.scss'
+import { UserContext } from '../../../../contexts/UserContext'
 
-const DoctorSideBar = () =>{
+const DoctorSideBar = ({current, setCurrent}) =>{
     const {Sider} = Layout
+    const {userLogout} = useContext(UserContext)
+    const handleMenuChange = (e) =>{
+        console.log('key' ,e.key)
+        setCurrent(e.key)
+    }
     return(
         <Sider>
-            <div>
-                Sider check
+            <div className="hospital-title">Swift Hospital</div>
+            <div className="sidebar-container">
+                <div className="profile-div">
+                    <div className="profile-info">
+                        <div className="profile-avatar">
+                            <Avatar icon={<UserOutlined />} size={64}/>
+                        </div>
+                        <div className="profile-content">
+                            <div className="welcome-title">
+                                Welcome
+                            </div>
+                            <div className="doctor-title">
+                                Dr.John
+                            </div>
+                            <div className="icons-row">
+                                <span className="icons"><MailOutlined className="icon"/></span>
+                                <span className="icons"><UserOutlined className="icon"/></span>
+                                <span className="icons"> <PhoneOutlined /> </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="report-container">
+                        <div className="report-heading">Today Report</div>
+                        <div className="report-info">
+                            <div className="report-box">
+                                16 Patient
+                            </div>
+                            <div className="report-box">
+                                20 Pending
+                            </div>
+                            <div className="report-box">
+                                04 Visit
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="menu-container">
+                    <div className="menu-title">MAIN NAVIGATION</div>
+                    <Menu className="sidebar-menu" selectedKeys={[current]} onClick={handleMenuChange}>
+                        <Menu.Item className="menu-item" key="dashboard">
+                            <HomeFilled /> <span className="menuItem-title">Dashboard</span>
+                        </Menu.Item>
+                        <Menu.Item className="menu-item" key="appointment"> 
+                            <CalendarFilled /> <span className="menuItem-title">Appointment</span>
+                        </Menu.Item>
+                        <Menu.Item className="menu-item" key="patients"> 
+                            <UserOutlined /> Patients
+                        </Menu.Item>
+                    </Menu>
+                </div>
+                <div className="logout-container">
+                    <Button onClick={()=>userLogout}>Logout</Button>
+                </div>
             </div>
         </Sider>
     )
