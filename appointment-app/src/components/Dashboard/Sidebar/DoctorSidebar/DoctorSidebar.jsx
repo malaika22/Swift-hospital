79b7f52d4de0432ab943cce00452ed3e67bb80fb
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-06-21 12:51:20 +0500

    Styled Doctor Sider

--- appointment-app/src/components/Dashboard/Sidebar/DoctorSidebar/DoctorSidebar.jsx
+++ appointment-app/src/components/Dashboard/Sidebar/DoctorSidebar/DoctorSidebar.jsx
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react'
 import { Layout, Avatar, Menu, Button } from 'antd'
 import {UserOutlined, MailOutlined, PhoneOutlined, HomeFilled, CalendarFilled} from '@ant-design/icons'
-import './styles.scss'
 import { UserContext } from '../../../../contexts/UserContext'
+import './styles.scss'
 
 const DoctorSideBar = ({current, setCurrent}) =>{
     const {Sider} = Layout
@@ -12,59 +12,61 @@ const DoctorSideBar = ({current, setCurrent}) =>{
         setCurrent(e.key)
     }
     return(
-        <Sider>
-            <div className="hospital-title">Swift Hospital</div>
-            <div className="sidebar-container">
-                <div className="profile-div">
-                    <div className="profile-info">
-                        <div className="profile-avatar">
-                            <Avatar icon={<UserOutlined />} size={64}/>
+        <Sider className="dashboardSider doctor-siderbar">
+            <div className="sider-main-container">
+                <div className="hospital-title">Swift Hospital</div>
+                <div className="sidebar-container">
+                    <div className="profile-div">
+                        <div className="profile-info">
+                            <div className="profile-avatar">
+                                <Avatar icon={<UserOutlined />} size={64}/>
+                            </div>
+                            <div className="profile-content">
+                                <div className="welcome-title">
+                                    Welcome
+                                </div>
+                                <div className="doctor-title">
+                                    Dr.John
+                                </div>
+                                <div className="icons-row">
+                                    <span className="icons"><MailOutlined className="icon"/></span>
+                                    <span className="icons"><UserOutlined className="icon"/></span>
+                                    <span className="icons"> <PhoneOutlined /> </span>
+                                </div>
+                            </div>
                         </div>
-                        <div className="profile-content">
-                            <div className="welcome-title">
-                                Welcome
-                            </div>
-                            <div className="doctor-title">
-                                Dr.John
-                            </div>
-                            <div className="icons-row">
-                                <span className="icons"><MailOutlined className="icon"/></span>
-                                <span className="icons"><UserOutlined className="icon"/></span>
-                                <span className="icons"> <PhoneOutlined /> </span>
+                        <div className="report-container">
+                            <div className="report-heading">Today Report</div>
+                            <div className="report-info">
+                                <div className="report-box">
+                                    16 Patient
+                                </div>
+                                <div className="report-box">
+                                    20 Pending
+                                </div>
+                                <div className="report-box">
+                                    04 Visit
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="report-container">
-                        <div className="report-heading">Today Report</div>
-                        <div className="report-info">
-                            <div className="report-box">
-                                16 Patient
-                            </div>
-                            <div className="report-box">
-                                20 Pending
-                            </div>
-                            <div className="report-box">
-                                04 Visit
-                            </div>
-                        </div>
+                    <div className="menu-container">
+                        <div className="menu-title">MAIN NAVIGATION</div>
+                        <Menu className="sidebar-menu" onClick={handleMenuChange} selectedKeys={[current]} defaultSelectedKeys={[current]}>
+                            <Menu.Item className="menu-item" key="dashboard">
+                                <HomeFilled className="menu-icon"/> <span className="menuItem-title">Dashboard</span>
+                            </Menu.Item>
+                            <Menu.Item className="menu-item" key="appointment"> 
+                                <CalendarFilled className="menu-icon"/> <span className="menuItem-title">Appointment</span>
+                            </Menu.Item>
+                            <Menu.Item className="menu-item" key="patients"> 
+                                <UserOutlined className="menu-icon"/> <span className="menuItem-title">Patients</span> 
+                            </Menu.Item>
+                        </Menu>
                     </div>
-                </div>
-                <div className="menu-container">
-                    <div className="menu-title">MAIN NAVIGATION</div>
-                    <Menu className="sidebar-menu" selectedKeys={[current]} onClick={handleMenuChange}>
-                        <Menu.Item className="menu-item" key="dashboard">
-                            <HomeFilled /> <span className="menuItem-title">Dashboard</span>
-                        </Menu.Item>
-                        <Menu.Item className="menu-item" key="appointment"> 
-                            <CalendarFilled /> <span className="menuItem-title">Appointment</span>
-                        </Menu.Item>
-                        <Menu.Item className="menu-item" key="patients"> 
-                            <UserOutlined /> Patients
-                        </Menu.Item>
-                    </Menu>
-                </div>
-                <div className="logout-container">
-                    <Button onClick={()=>userLogout}>Logout</Button>
+                    <div className="logout-container">
+                        <Button onClick={()=>userLogout()}>Logout</Button>
+                    </div>
                 </div>
             </div>
         </Sider>
