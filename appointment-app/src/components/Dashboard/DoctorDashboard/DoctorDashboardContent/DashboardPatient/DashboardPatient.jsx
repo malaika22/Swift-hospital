2386cf7ac4fdcc5510a5540ccc6c5162dd711a45
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-06-22 16:39:43 +0500

    Styled DashboardPatient

--- appointment-app/src/components/Dashboard/DoctorDashboard/DoctorDashboardContent/DashboardPatient/DashboardPatient.jsx
+++ appointment-app/src/components/Dashboard/DoctorDashboard/DoctorDashboardContent/DashboardPatient/DashboardPatient.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Avatar } from 'antd';
+import {UserOutlined} from '@ant-design/icons';
+import { UserContext } from '../../../../../contexts/UserContext'
+import './styles.scss'
 
 const DashboardPatient = () =>{
+    const {docPatients} = useContext(UserContext)
+    console.log('doc patients', docPatients)
     return(
-        <div>
-            Dashboard Patient
+        <div className="doctor-patients-container">
+            <div className="dashboard-header">
+                <div className="dashboard-title">All Patients</div>
+                <div className="welcome-div">Welcome to Swift Application</div>
+            </div>
+            <div className="patients-container">
+                {(docPatients || []).map((data)=>{
+                    const patnt = (data || {}).patient
+                    return(
+                        <div className="patient-card">
+                            <div className="patient-avatar">
+                                <Avatar icon={<UserOutlined/>} size={64}/>
+                            </div>
+                            <div className="patient-info">
+                                <div className="patient-name">{patnt.name}</div>
+                                <div className="patient-contactNumber">{patnt.contactNumber}</div>
+                                <div className="patient-email">{patnt.email}</div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
