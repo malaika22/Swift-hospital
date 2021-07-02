bcb364c178355bb8fb9f705cfa9013eedfa683cc
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-07-02 17:23:51 +0500

    Implemented Admin Login in ContextApi

--- appointment-app/src/contexts/UserContext.js
+++ appointment-app/src/contexts/UserContext.js
@@ -11,12 +11,25 @@ export const UserContextProvider = ({children}) =>{
     const [docPatients , setDocPatients] = useState([])
     const [sessionUser , setSessionUser] = useState({})
     const [loading, setLoading] = useState(true)
+    const [role, setRole] = useState('')
     const history = useHistory()
+    
     useEffect(()=>{
         console.log('get token', getToken())
         if(getToken()[0] && getToken()[1]){
             const [storageData] = getToken()
-            userLogin(storageData)
+            console.log('in codition',currentUser, storageData)
+            if(storageData.role==='doctor'){
+                userLogin({
+                    email: storageData.email,
+                    password: storageData.password
+                })
+            } else if(storageData.role==="admin"){
+                adminLogin({
+                    email: storageData.email,
+                    password: storageData.password
+                })
+            }
         } else{
             //setLoading('no user')
             history.push("/login")
@@ -38,7 +51,7 @@ export const UserContextProvider = ({children}) =>{
             if(res.status === 200){
                 console.log("in success status", res.data.data.user)
                 setCurrentUser(res.data.data.user)
-                setUserSession(credentials , true)
+                setUserSession(res.data.data.user , true)
                 getDoctorPatients()
                 setLoading(false)  
             }
@@ -108,6 +121,55 @@ export const UserContextProvider = ({children}) =>{
         }
     }
 
+    // Admin Context
+    const adminLogin = async (credentials) => {
+        //history.push("/")
+        try {
+            const res = await axios({
+                method: 'post',
+                url: 'https://datamansys.herokuapp.com/api/v1/admin/login-admin',
+                data: {
+                    email: credentials.email,
+                    password: credentials.password
+                },
+                withCredentials: true
+            });
+            if (res.status === 200) {
+                console.log("in success status", res.data.data.user)
+                setCurrentUser(res.data.data.user)
+                setUserSession(res.data.data.user, true)
+                setLoading(false)
+            }
+        } catch (err) {
+            console.log('login error', err)
+            setLoading(true)
+            history.push("/login")
+        }
+    }
+
+    const adminLogout = async () =>{
+        console.log('user logout')
+        setLoading(true)
+            try{
+                const res = await axios.get("https://datamansys.herokuapp.com/api/v1/admin/logout")
+                    console.log('res', res)
+                    console.log(typeof(res.data.status))
+                if(res.data.status==="succesfully loggedout") {
+                    console.log("in condition")
+                    removeUserSession()
+                    setCurrentUser({})
+                    setLoading(false)
+                    history.push("/login")
+                }
+            } catch(err) {
+                console.log("sign out error", err)
+            }
+    }
+
+    
+
+
+
     return(
         <UserContext.Provider value={{
             currentUser,
@@ -116,7 +178,10 @@ export const UserContextProvider = ({children}) =>{
             userLogout : userLogout ,
             docPatients : docPatients ,
             loading: loading,
-            sessionUser : sessionUser
+            sessionUser : sessionUser,
+            setRole: setRole,
+            adminLogin : adminLogin,
+            role: role
         }}>
             {children}
         </UserContext.Provider>
