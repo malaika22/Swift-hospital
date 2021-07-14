a64e5ad01dc98b1576e09bba962a55232a141a9b
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-07-14 16:47:29 +0500

    Updated Dashboard

--- appointment-app/src/components/Dashboard/Dashboard.jsx
+++ appointment-app/src/components/Dashboard/Dashboard.jsx
@@ -4,6 +4,7 @@ import {Redirect, useHistory} from 'react-router-dom'
 import { UserContext } from '../../contexts/UserContext'
 import DoctorDashboard from './DoctorDashboard/DoctorDashboard'
 import LoadSpinner from '../LoadSpinner/LoadSpinner'
+import AdminDashboard from './AdminDashboard/AdminDashboard'
 
 const Dashboard = () =>{
     const {currentUser, loading} = useContext(UserContext)
@@ -16,7 +17,11 @@ const Dashboard = () =>{
     else
     return(
         <div className="dashboard-container" style={{boxSizing: "border-box"}}>
-            <DoctorDashboard />
+            {currentUser || {}.role === "admin" ? 
+                <AdminDashboard /> : 
+                <DoctorDashboard />
+            }
+           
         </div>
     )
 }
