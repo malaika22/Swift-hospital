f6cde034277cbddd503d50d4fee3c68f62b166f8
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-07-14 16:47:15 +0500

    Added Admin Sidebar

---
+++ appointment-app/src/components/Dashboard/Sidebar/AdminSidebar/AdminSidebar.jsx
@@ -0,0 +1,77 @@
+import React, { useContext } from 'react'
+import { Layout, Avatar, Menu, Button } from 'antd'
+import {UserOutlined, MailOutlined, PhoneOutlined, HomeFilled, CalendarFilled} from '@ant-design/icons'
+import { UserContext } from '../../../../contexts/UserContext'
+import './styles.scss'
+
+const AdminSideBar = ({current, setCurrent}) =>{
+    const {Sider} = Layout
+    const {userLogout} = useContext(UserContext)
+    const handleMenuChange = (e) =>{
+        console.log('key' ,e.key)
+        setCurrent(e.key)
+    }
+    console.log('menu current', current)
+    return(
+        <Sider className="dashboardSider doctor-siderbar">
+            <div className="sider-main-container">
+                <div className="hospital-title">Swift Hospital</div>
+                <div className="sidebar-container">
+                    <div className="profile-div">
+                        <div className="profile-info">
+                            <div className="profile-avatar">
+                                <Avatar icon={<UserOutlined />} size={64}/>
+                            </div>
+                            <div className="profile-content">
+                                <div className="welcome-title">
+                                    Welcome
+                                </div>
+                                <div className="doctor-title">
+                                    John
+                                </div>
+                                <div className="icons-row">
+                                    <span className="icons"><MailOutlined className="icon"/></span>
+                                    <span className="icons"><UserOutlined className="icon"/></span>
+                                    <span className="icons"> <PhoneOutlined /> </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="report-container">
+                            <div className="report-heading">Today Report</div>
+                            <div className="report-info">
+                                <div className="report-box">
+                                    16 Patient
+                                </div>
+                                <div className="report-box">
+                                    20 Pending
+                                </div>
+                                <div className="report-box">
+                                    04 Visit
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="menu-container">
+                        <div className="menu-title">MAIN NAVIGATION</div>
+                        <Menu className="sidebar-menu" onClick={handleMenuChange} selectedKeys={[current]} defaultSelectedKeys={[current]}>
+                            <Menu.Item className="menu-item" key="dashboard">
+                                <HomeFilled className="menu-icon"/> <span className="menuItem-title">Dashboard</span>
+                            </Menu.Item>
+                            <Menu.Item className="menu-item" key="appointment"> 
+                                <CalendarFilled className="menu-icon"/> <span className="menuItem-title">Appointment</span>
+                            </Menu.Item>
+                            <Menu.Item className="menu-item" key="patients"> 
+                                <UserOutlined className="menu-icon"/> <span className="menuItem-title">Patients</span> 
+                            </Menu.Item>
+                        </Menu>
+                    </div>
+                    <div className="logout-container">
+                        <Button onClick={()=>userLogout()}>Logout</Button>
+                    </div>
+                </div>
+            </div>
+        </Sider>
+    )
+}
+
+export default AdminSideBar
