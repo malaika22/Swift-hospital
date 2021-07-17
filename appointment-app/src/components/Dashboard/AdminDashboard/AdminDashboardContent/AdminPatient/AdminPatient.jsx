8ab7bd3f241a5106038bc206ae7f45708470cb92
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-07-17 16:18:10 +0500

    Update AdminPatient.jsx

--- appointment-app/src/components/Dashboard/AdminDashboard/AdminDashboardContent/AdminPatient/AdminPatient.jsx
+++ appointment-app/src/components/Dashboard/AdminDashboard/AdminDashboardContent/AdminPatient/AdminPatient.jsx
@@ -3,6 +3,7 @@ import { Avatar } from 'antd';
 import {UserOutlined} from '@ant-design/icons';
 import { UserContext } from '../../../../../contexts/UserContext'
 import './styles.scss'
+import PatientCard from './PatientCard/PatientCard';
 
 const AdminPatient = () =>{
     const {adminPatients} = useContext(UserContext)
@@ -16,16 +17,7 @@ const AdminPatient = () =>{
             <div className="patients-container">
                 {(adminPatients || []).map((data)=>{
                     return(
-                        <div className="patient-card">
-                            <div className="patient-avatar">
-                                <Avatar icon={<UserOutlined/>} size={64}/>
-                            </div>
-                            <div className="patient-info">
-                                <div className="patient-name">{data.name}</div>
-                                <div className="patient-contactNumber">{data.contactNumber}</div>
-                                <div className="patient-email">{data.email}</div>
-                            </div>
-                        </div>
+                        <PatientCard patnt={data} />
                     )
                 })}
             </div>
